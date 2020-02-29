68c7ce4710066c3bf7e7ccf8e9a7fee46e7b6e16
Patrick <patrick.biedrzycki@tigerspike.com>
2020-02-29 20:41:46 +1100

    Clean

--- Examples_iOS/SuperpoweredFrequencies/SuperpoweredFrequencies/SuperpoweredBridge.h
+++
@@ -1,9 +0,0 @@
-//  The public Objective-C++ class we expose to Swift.
-
-#import <Foundation/Foundation.h>
-
-@interface SuperpoweredBridge: NSObject
-
-- (void)getFrequencies:(float *)freqs;
-
-@end
